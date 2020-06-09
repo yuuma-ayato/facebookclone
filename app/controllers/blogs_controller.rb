286ebceb2abadcb6441836c19619a3dc906af090
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-06-09 17:15:57 +0900

    ログイン機能作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
   def create
-    @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
     if params[:back]
       render :new
     else
@@ -34,7 +34,7 @@ class BlogsController < ApplicationController
     redirect_to blogs_path, notice:"ブログを削除しました！"
   end
   def confirm
-    @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
     render :new if @blog.invalid?
   end
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'blogs#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   resources :blogs do
     collection do
       post :confirm
